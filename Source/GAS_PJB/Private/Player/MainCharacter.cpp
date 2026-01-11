ade2b0ead14161e4fe32e9a8199b3fdc9c0f6085
Jun-Beom Park <101052774+jbaem@users.noreply.github.com>
2026-01-11 12:02:25 +0900

    [feat] 플레이어 입력 바인딩

--- Source/GAS_PJB/Private/Player/MainCharacter.cpp
+++ Source/GAS_PJB/Private/Player/MainCharacter.cpp
@@ -5,6 +5,10 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
 AMainCharacter::AMainCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -42,5 +46,56 @@ void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComp->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMainCharacter::OnMove);
+		EnhancedInputComp->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMainCharacter::OnLook);
+		
+		EnhancedInputComp->BindAction(Ability1Action, ETriggerEvent::Started, this, &AMainCharacter::OnAbility1Pressed);
+	}
 }
 
+void AMainCharacter::DoMove(float Right, float Forward)
+{
+	if (!GetController())
+	{
+		return;
+	}
+
+	const FRotator Rotation = GetController()->GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection, Forward);
+	AddMovementInput(RightDirection, Right);
+}
+
+void AMainCharacter::DoLook(float Yaw, float Pitch)
+{
+	if (!GetController())
+	{
+		return;
+	}
+
+	AddControllerYawInput(Yaw);
+	AddControllerPitchInput(Pitch);
+}
+
+void AMainCharacter::OnMove(const FInputActionValue & Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	DoMove(MovementVector.X, MovementVector.Y);
+}
+
+void AMainCharacter::OnLook(const FInputActionValue & Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+	DoLook(LookAxisVector.X, LookAxisVector.Y);
+}
+
+void AMainCharacter::OnAbility1Pressed()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Ability 1 Pressed"));
+}
--- Source/GAS_PJB/Public/Player/MainCharacter.h
+++ Source/GAS_PJB/Public/Player/MainCharacter.h
@@ -20,9 +20,28 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+public:
+	UFUNCTION(BlueprintCallable, Category = "Main|Character")
+	void DoMove(float Right, float Forward);
+	UFUNCTION(BlueprintCallable, Category = "Main|Character")
+	void DoLook(float Yaw, float Pitch);
+
+protected:
+	void OnMove(const struct FInputActionValue& Value);
+	void OnLook(const struct FInputActionValue& Value);
+	void OnAbility1Pressed();
+
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Main|Camera")
 	TObjectPtr<class USpringArmComponent> CameraBoom;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Main|Camera")
 	TObjectPtr<class UCameraComponent> FollowCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Main|Input")
+	TObjectPtr<class UInputAction> MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Main|Input")
+	TObjectPtr<class UInputAction> LookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Main|Input")
+	TObjectPtr<class UInputAction> Ability1Action;
+
 };
